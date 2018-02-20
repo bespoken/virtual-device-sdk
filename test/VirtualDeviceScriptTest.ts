a8dff05edd1d2124e9070ed7b9ebead2d7f1a81a
John Kelvie <jpk@bespoken.io>
2018-02-20 12:38:58 -0800

    Added support for wildcards
    Also, forced use of mocks in more tests

--- test/VirtualDeviceScriptTest.ts
+++ test/VirtualDeviceScriptTest.ts
@@ -223,10 +223,10 @@ describe("VirtualDeviceScript", function() {
         it("success", async () => {
             const tests = [`
 "open test player":
-  transcript: "welcome to the simple audio player"
+  transcript: "welcome to the si*le audio* player"
   card:
     imageURL: "https://bespoken.io/wp-content/uploads/Bespoken-Logo-Web-White-e1500590667994.png"
-    subTitle: "Simple Player Unit Test"
+    subTitle: "Simple Player * Test"
     mainTitle: "Title of the card"
     textField: "Text content for a standard card"
     type: "BodyTemplate2"
@@ -543,6 +543,7 @@ describe("VirtualDeviceScript", function() {
                     `&user_id=${userID}`)
                 .reply(200, "AUTHORIZED");
             sevCheckAuthSpy = Sinon.spy(VirtualDeviceScript.prototype, "checkAuth");
+            MessageMock.enable();
         });
 
         after(() => {
@@ -550,6 +551,7 @@ describe("VirtualDeviceScript", function() {
             nock.cleanAll();
             sevCheckAuthSpy.reset();
             sevCheckAuthSpy.restore();
+            MessageMock.disable();
         });
 
         it("success", async () => {
@@ -567,10 +569,12 @@ describe("VirtualDeviceScript", function() {
         before(() => {
             checkAuthStub = Sinon.stub(VirtualDeviceValidator.prototype, "checkAuth")
                 .returns(Promise.resolve("AUTHORIZED"));
+            MessageMock.enable();
         });
 
         after(() => {
             checkAuthStub.restore();
+            MessageMock.disable();
         });
 
         it("success", async () => {
