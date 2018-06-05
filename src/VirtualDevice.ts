7b9e5801aed95ab165bc3cd2d5fa6b0e666394cd
Diego Martín <dmarvp@hotmail.com>
2018-06-05 18:25:16 -0500

    Removes unneeded code

--- src/VirtualDevice.ts
+++ src/VirtualDevice.ts
@@ -58,7 +58,6 @@ export class VirtualDevice {
                 response.on("end", () => {
                     if (response.statusCode === 200) {
                         const result: IVirtualDeviceResult = JSON.parse(data);
-                        result.transcript = result.transcript;
                         result.message = message;
                         resolve(result);
                     } else {
@@ -147,12 +146,7 @@ export class VirtualDevice {
 
     private handleBatchResponse(data: string): IVirtualDeviceResult[] {
         const json = JSON.parse(data);
-        const results = [];
-        for (const result of json.results) {
-            result.transcript = result.transcript;
-            results.push(result);
-        }
-        return results;
+        return json.results;
     }
 }
 
